d3d468b1c4a55debd332c304cc1529a79dd2b2c3
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-07 11:28:59 -0500

    remove downloadToFile

--- src/create_ui.js
+++ src/create_ui.js
@@ -232,20 +232,6 @@ function ui_present() {
   return select('#id_panX');
 }
 
-// https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
-function downloadToFile(filename, text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-
-  element.style.display = 'none';
-  document.body.appendChild(element);
-
-  element.click();
-
-  document.body.removeChild(element);
-}
-
 // https://unicode.org/charts/nameslist/n_2190.html
 // Arrows
 
